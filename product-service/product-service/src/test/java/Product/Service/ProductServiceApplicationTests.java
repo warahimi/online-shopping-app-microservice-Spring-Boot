072eecb8caed4b836de7c62bcb38ae670a4cb805
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-08-31 17:23:26 -0700

    Product Service Set up

---
+++ product-service/product-service/src/test/java/Product/Service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Product.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
